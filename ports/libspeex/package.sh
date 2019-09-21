d1d1da1c7d83b7b1a82db0f7aa335561df29b280
Eric Dodd <eric.e.dodd@gmail.com>
2019-09-20 20:39:58 -0400

    errors

---
+++ ports/libspeex/package.sh
@@ -0,0 +1,5 @@
+#!/opt/local/bin/mksh ../../.port.sh
+port=speex
+version=1.2.0
+useconfigure=true
+files="https://ftp.osuosl.org/pub/xiph/releases/speex/speex-1.2.0.tar.gz speex-1.2.0.tar.gz 18ebc3fa3236b4369509e9439acc32d0e864fa7f"
---
+++ ports/libtheora/package.sh
@@ -0,0 +1,5 @@
+#!/opt/local/bin/mksh ../../.port.sh
+port=libtheora
+version=1.1.1
+useconfigure=true
+files="https://ftp.osuosl.org/pub/xiph/releases/theora/libtheora-1.1.1.tar.bz2 libtheora-1.1.1.tar.bz2 8dcaa8e61cd86eb1244467c0b64b9ddac04ae262"
