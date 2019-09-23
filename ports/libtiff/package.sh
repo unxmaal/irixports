9ea0fcdcbdb743bf579a809dd19cfbcf889d0cac
Eric Dodd <eric.e.dodd@gmail.com>
2019-09-22 20:19:07 -0400

    libtiff, not working

---
+++ ports/libtiff/package.sh
@@ -0,0 +1,5 @@
+#!/opt/local/bin/mksh ../../.port.sh
+port=tiff
+version=4.0.10
+useconfigure=true
+files="http://download.osgeo.org/libtiff/tiff-4.0.10.tar.gz tiff-4.0.10.tar.gz c783b80f05cdacf282aa022dc5f5b0ede5e021ae"
