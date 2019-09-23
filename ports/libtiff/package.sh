d79dd7d26ba027d85b92254c3a4534787d50a29d
Eric Dodd <eric.e.dodd@gmail.com>
2019-09-23 19:15:05 -0400

    errors after libjpeg

--- ports/libtiff/package.sh
+++ ports/libtiff/package.sh
@@ -2,4 +2,10 @@
 port=tiff
 version=4.0.10
 useconfigure=true
+configopts="JPEG_INCLUDE_DIR=$prefix/include JPEG_LIBRARY=$prefix/lib"
 files="http://download.osgeo.org/libtiff/tiff-4.0.10.tar.gz tiff-4.0.10.tar.gz c783b80f05cdacf282aa022dc5f5b0ede5e021ae"
+
+configure(){
+        runwd ./autogen.sh
+        runwd ./configure --prefix="$prefix" $configopts
+}
